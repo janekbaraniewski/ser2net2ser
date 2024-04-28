221e1b02a4648ebe5731156d2ad148d92801ecb9
Jan Baraniewski <dev@baraniewski.com>
2024-04-28 21:09:39 +0200

    Reorder includes

--- include/common.hpp
+++ include/common.hpp
@@ -1,15 +1,16 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
-#include <boost/program_options.hpp>
 #include <iostream>
 #include <string>
 #include <array>
 #include <fcntl.h>
 #include <unistd.h>
 #include <termios.h>
+
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/program_options.hpp>
 
 #include "logging.h"
 
