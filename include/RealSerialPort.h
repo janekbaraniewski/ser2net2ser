86f15fca4cfd31dc198ce321f73de0ee30cdfa06
Jan Baraniewski <dev@baraniewski.com>
2024-04-29 20:02:47 +0200

    Some more logs

--- include/RealSerialPort.h
+++ include/RealSerialPort.h
@@ -12,6 +12,7 @@ public:
     explicit RealSerialPort(boost::asio::io_service& io) : port(io) {}
 
     void open(const std::string& device, serial_port_base::baud_rate baudRate) override {
+        BOOST_LOG_TRIVIAL(info) << "Setting up connection to serial port " << device;
         try {
             port.open(device);
             setSerialOptions(baudRate);
@@ -21,6 +22,7 @@ public:
     }
 
     void setSerialOptions(serial_port_base::baud_rate baudRate) {
+        BOOST_LOG_TRIVIAL(info) << "Setting connection options.";
         port.set_option(baudRate);
         port.set_option(serial_port_base::character_size(8));
         port.set_option(serial_port_base::parity(serial_port_base::parity::none));
@@ -29,6 +31,7 @@ public:
     }
 
     void async_read_some(const boost::asio::mutable_buffer& buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler) override {
+        BOOST_LOG_TRIVIAL(info) << "Read some.";
         if (!port.is_open()) {
             BOOST_LOG_TRIVIAL(error) << "Attempt to read from a closed serial port.";
             return;
@@ -37,6 +40,7 @@ public:
     }
 
     void async_write(const boost::asio::const_buffer& buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler) override {
+        BOOST_LOG_TRIVIAL(info) << "write some some.";
         if (!port.is_open()) {
             BOOST_LOG_TRIVIAL(error) << "Attempt to write to a closed serial port.";
             return;
