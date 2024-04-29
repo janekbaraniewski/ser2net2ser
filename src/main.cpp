2f26621f1094ff2ae78bbe898c69586f88818b8b
Jan Baraniewski <dev@baraniewski.com>
2024-04-29 13:40:35 +0200

    Finish switch to single binary with subcommands

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,21 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+void setup_and_run_server(io_service& io, const variables_map& vm) {
+    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), vm["port"].as<unsigned short>()));
+    RealSerialPort realSerial(io);
+    realSerial.open(vm["device"].as<string>());
+    realSerial.set_option(serial_port_base::baud_rate(vm["baud"].as<unsigned int>()));
+
+    SerialServer server(io, realSerial, acceptor);
+    server.run();
+}
+
+void setup_and_run_client(io_service& io, const variables_map& vm) {
+    SerialClient client(io, vm["server"].as<string>(), vm["port"].as<unsigned short>(), vm["vsp"].as<string>());
+    client.run();
+}
+
 int main(int argc, char* argv[]) {
     init_logging();
 
@@ -22,60 +37,49 @@ int main(int argc, char* argv[]) {
     }
 
     string command = argv[1];
+    io_service io;
 
-    if (command == "serve") {
-        options_description serve_desc("Server options");
-        serve_desc.add_options()
-            ("help,h", "Display this help message")
-            ("device,d", value<string>()->default_value("/dev/ttyUSB0"), "Device name")
-            ("baud,b", value<unsigned int>()->default_value(9600), "Baud rate")
-            ("port,p", value<unsigned short>()->default_value(12345), "Port number");
+    try {
+        if (command == "serve") {
+            options_description serve_desc("Server options");
+            serve_desc.add_options()
+                ("help,h", "produce help message")
+                ("device,d", value<string>()->default_value("/dev/ttyUSB0"), "Device name")
+                ("baud,b", value<unsigned int>()->default_value(9600), "Baud rate")
+                ("port,p", value<unsigned short>()->default_value(12345), "Port number");
+            variables_map vm;
+            store(parse_command_line(argc, argv, serve_desc), vm);
 
-        variables_map serve_vm;
-        store(parse_command_line(argc - 1, argv + 1, serve_desc), serve_vm);
+            if (vm.count("help")) {
+                cout << serve_desc << endl;
+                return 0;
+            }
 
-        if (serve_vm.count("help")) {
-            cout << serve_desc << endl;
-            return 0;
+            notify(vm);
+            setup_and_run_server(io, vm);
+        } else if (command == "connect") {
+            options_description connect_desc("Client options");
+            connect_desc.add_options()
+                ("help,h", "produce help message")
+                ("server,s", value<string>()->default_value("127.0.0.1"), "Server IP address")
+                ("port,p", value<unsigned short>()->default_value(12345), "Server port")
+                ("vsp,v", value<string>()->required(), "Virtual serial port name");
+            variables_map vm;
+            store(parse_command_line(argc, argv, connect_desc), vm);
+
+            if (vm.count("help")) {
+                cout << connect_desc << endl;
+                return 0;
+            }
+
+
+            notify(vm);
+            setup_and_run_client(io, vm);
+        } else {
+            throw std::invalid_argument("Invalid command provided. Use 'serve' or 'connect'.");
         }
-
-        notify(serve_vm);
-
-        io_service io;
-        tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), serve_vm["port"].as<unsigned short>()));
-        RealSerialPort realSerial(io);
-        realSerial.open(serve_vm["device"].as<string>());
-        realSerial.set_option(serial_port_base::baud_rate(serve_vm["baud"].as<unsigned int>()));
-
-        SerialServer server(io, realSerial, acceptor);
-        server.run();
-    } else if (command == "connect") {
-        options_description connect_desc("Client options");
-        connect_desc.add_options()
-            ("help,h", "Display this help message")
-            ("server,s", value<string>()->default_value("127.0.0.1"), "Server IP address")
-            ("port,p", value<unsigned short>()->default_value(12345), "Server port")
-            ("vsp,v", value<string>()->required(), "Virtual serial port name");
-
-        variables_map connect_vm;
-        store(parse_command_line(argc - 1, argv + 1, connect_desc), connect_vm);
-
-        if (connect_vm.count("help")) {
-            cout << connect_desc << endl;
-            return 0;
-        }
-
-        notify(connect_vm);
-
-        string server_ip = connect_vm["server"].as<string>();
-        unsigned short server_port = connect_vm["port"].as<unsigned short>();
-        string vsp_name = connect_vm["vsp"].as<string>();
-
-        io_service io;
-        SerialClient client(io, server_ip, server_port, vsp_name);
-        client.run();
-    } else {
-        cerr << "Invalid command. Use 'serve' or 'connect'." << endl;
+    } catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
 
