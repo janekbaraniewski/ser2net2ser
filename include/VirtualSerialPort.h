5b649c4ceae91b2f8ce676ff62e87dbd501dbc4d
Jan Baraniewski <dev@baraniewski.com>
2024-04-29 20:36:05 +0200

    Rewrite communication in client

--- include/VirtualSerialPort.h
+++ include/VirtualSerialPort.h
@@ -1,6 +1,7 @@
 #ifndef VIRTUALSERIALPORT_H
 #define VIRTUALSERIALPORT_H
 
+#include <boost/asio.hpp>
 #include <string>
 #include <functional>
 #include <fcntl.h>
@@ -20,16 +21,18 @@
 
 class VirtualSerialPort {
 public:
-    VirtualSerialPort(const std::string& device);
+    VirtualSerialPort(boost::asio::io_context& io_context, const std::string& device);
     ~VirtualSerialPort();
 
     void close();
-    bool write(const std::string& data);
-    std::string read();
+    void async_read(boost::asio::mutable_buffer buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler);
+    void async_write(boost::asio::const_buffer buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler);
+
+    // bool write(const std::string& data);
+    // std::string read();
 
 private:
-    int master_fd_;
-    int slave_fd_;
+    boost::asio::posix::stream_descriptor master_fd_;
     std::string device_name_;
 };
 
--- src/VirtualSerialPort.cpp
+++ src/VirtualSerialPort.cpp
@@ -1,12 +1,13 @@
 #include "VirtualSerialPort.h"
 
-VirtualSerialPort::VirtualSerialPort(const std::string& device) : device_name_("/dev/" + device) {
-    master_fd_ = posix_openpt(O_RDWR | O_NOCTTY);
-    if (master_fd_ == -1 || grantpt(master_fd_) != 0 || unlockpt(master_fd_) != 0) {
+VirtualSerialPort::VirtualSerialPort(boost::asio::io_context& io_context, const std::string& device)
+    : master_fd_(io_context), device_name_("/dev/" + device) {
+    int master_fd = posix_openpt(O_RDWR | O_NOCTTY);
+    if (master_fd == -1 || grantpt(master_fd) != 0 || unlockpt(master_fd) != 0) {
         throw std::runtime_error("Failed to open PTY master");
     }
 
-    char* slave_name = ptsname(master_fd_);
+    char* slave_name = ptsname(master_fd);
     if (!slave_name) {
         throw std::runtime_error("Failed to get PTY slave name");
     }
@@ -20,86 +21,65 @@ VirtualSerialPort::VirtualSerialPort(const std::string& device) : device_name_("
     }
 
     chmod(device_name_.c_str(), 0660);
-    chown(device_name_.c_str(), -1, getgrnam("tty")->gr_gid);  // Set group to 'tty'
+    chown(device_name_.c_str(), -1, getgrnam("tty")->gr_gid);
 
-    slave_fd_ = ::open(slave_name, O_RDWR);
-    if (slave_fd_ == -1) {
-        throw std::runtime_error("Failed to open PTY slave");
-    }
-
-    // Configure terminal settings
-    struct termios tty;
-    memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(slave_fd_, &tty) != 0) {
-        throw std::runtime_error("Failed to get terminal attributes");
-    }
-
-    cfmakeraw(&tty);  // Make the terminal raw
-    tty.c_cflag |= CREAD | CLOCAL;  // Enable receiver, Ignore modem control lines
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;  // 8-bit characters
-    tty.c_cflag &= ~PARENB;  // No parity bit
-    tty.c_cflag &= ~CSTOPB;  // 1 stop bit
-    tty.c_cflag &= ~CRTSCTS;  // No hardware flow control
-    cfsetispeed(&tty, B115200);  // Input speed
-    cfsetospeed(&tty, B115200);  // Output speed
-
-    if (tcsetattr(slave_fd_, TCSANOW, &tty) != 0) {
-        throw std::runtime_error("Failed to set terminal attributes");
-    }
+    master_fd_.assign(master_fd);
 }
+
+
 
 VirtualSerialPort::~VirtualSerialPort() {
     close();
 }
 
 void VirtualSerialPort::close() {
-    if (master_fd_ != -1) {
-        ::close(master_fd_);
-        master_fd_ = -1;
-    }
-    if (slave_fd_ != -1) {
-        ::close(slave_fd_);
-        slave_fd_ = -1;
-    }
+    master_fd_.close();
     unlink(device_name_.c_str());
 }
 
-bool VirtualSerialPort::write(const std::string& data) {
-    if (master_fd_ == -1) {
-        BOOST_LOG_TRIVIAL(error) << "Attempt to write with closed master FD";
-        return false;
-    }
-
-    ssize_t written = 0;
-    ssize_t to_write = data.size();
-    while (written < to_write) {
-        ssize_t result = ::write(master_fd_, data.c_str() + written, to_write - written);
-        if (result < 0) {
-            BOOST_LOG_TRIVIAL(error) << "Write error: " << strerror(errno);
-            return false;
-        }
-        written += result;
-    }
-    return true;
+void VirtualSerialPort::async_read(boost::asio::mutable_buffer buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler) {
+    boost::asio::async_read(master_fd_, buffer, handler);
 }
 
-std::string VirtualSerialPort::read() {
-    if (master_fd_ == -1) {
-        BOOST_LOG_TRIVIAL(error) << "Attempt to read with closed master FD";
-        return "";
-    }
-
-    char buffer[256];
-    ssize_t len = ::read(master_fd_, buffer, sizeof(buffer) - 1);
-    if (len < 0) {
-        BOOST_LOG_TRIVIAL(error) << "Read error: " << strerror(errno);
-        return "";
-    } else if (len == 0) {
-        BOOST_LOG_TRIVIAL(info) << "No data read";
-        return "";
-    }
-
-    buffer[len] = '\0';
-    return std::string(buffer, len);
+void VirtualSerialPort::async_write(boost::asio::const_buffer buffer, std::function<void(const boost::system::error_code&, std::size_t)> handler) {
+    boost::asio::async_write(master_fd_, buffer, handler);
 }
+
+// bool VirtualSerialPort::write(const std::string& data) {
+//     if (master_fd_ == -1) {
+//         BOOST_LOG_TRIVIAL(error) << "Attempt to write with closed master FD";
+//         return false;
+//     }
+
+//     ssize_t written = 0;
+//     ssize_t to_write = data.size();
+//     while (written < to_write) {
+//         ssize_t result = ::write(master_fd_, data.c_str() + written, to_write - written);
+//         if (result < 0) {
+//             BOOST_LOG_TRIVIAL(error) << "Write error: " << strerror(errno);
+//             return false;
+//         }
+//         written += result;
+//     }
+//     return true;
+// }
+
+// std::string VirtualSerialPort::read() {
+//     if (master_fd_ == -1) {
+//         BOOST_LOG_TRIVIAL(error) << "Attempt to read with closed master FD";
+//         return "";
+//     }
+
+//     char buffer[256];
+//     ssize_t len = ::read(master_fd_, buffer, sizeof(buffer) - 1);
+//     if (len < 0) {
+//         BOOST_LOG_TRIVIAL(error) << "Read error: " << strerror(errno);
+//         return "";
+//     } else if (len == 0) {
+//         BOOST_LOG_TRIVIAL(info) << "No data read";
+//         return "";
+//     }
+
+//     buffer[len] = '\0';
+//     return std::string(buffer, len);
+// }
