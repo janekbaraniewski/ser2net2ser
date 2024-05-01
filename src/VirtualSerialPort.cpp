5f6f84bf8cc0ea26db47b7be3db26720fd0453ec
Jan Baraniewski <dev@baraniewski.com>
2024-05-01 22:07:50 +0200

    WIP - update serial initialization

--- src/VirtualSerialPort.cpp
+++ src/VirtualSerialPort.cpp
@@ -3,59 +3,49 @@
 VirtualSerialPort::VirtualSerialPort(boost::asio::io_context& io_context, const std::string& device)
     : master_fd_(io_context), slave_fd_(io_context), device_name_("/dev/" + device) {
     std::lock_guard<std::mutex> lock(mutex_);
+    char *slave_name;
     int master_fd = posix_openpt(O_RDWR | O_NOCTTY);
     if (master_fd == -1) {
         BOOST_LOG_TRIVIAL(error) << "Failed to open PTY master: " << strerror(errno);
         throw std::runtime_error("Failed to open PTY master");
     }
-        BOOST_LOG_TRIVIAL(info) << "PTY master opened successfully";
+    BOOST_LOG_TRIVIAL(info) << "PTY master opened successfully";
 
-        if (grantpt(master_fd) != 0 || unlockpt(master_fd) != 0) {
+    if (grantpt(master_fd) == -1 || unlockpt(master_fd) == -1 || (slave_name = ptsname(master_fd)) == nullptr) {
             BOOST_LOG_TRIVIAL(error) << "Failed to grant or unlock PTY: " << strerror(errno);
             throw std::runtime_error("Failed to grant or unlock PTY");
-        }
-        BOOST_LOG_TRIVIAL(info) << "PTY grant and unlock successful";
+    }
+    BOOST_LOG_TRIVIAL(info) << "PTY grant and unlock successful";
+    BOOST_LOG_TRIVIAL(info) << "Slave PTY name: " << slave_name << std::endl;
 
-        char* slave_name = ptsname(master_fd);
-        if (!slave_name) {
-            BOOST_LOG_TRIVIAL(error) << "Failed to get PTY slave name";
-            throw std::runtime_error("Failed to get PTY slave name");
-        }
+    // Attempt to create a symbolic link from slave_name to "/dev/ttyUSB0"
+    if (symlink(slave_name, device_name_.c_str()) == -1) {
+        BOOST_LOG_TRIVIAL(error) << "Failed to create symlink for PTY slave: " << strerror(errno);
+        throw std::runtime_error("Failed to create symlink for PTY slave");
+    }
+    BOOST_LOG_TRIVIAL(info) << "Symlink for PTY slave created successfully";
 
-        if (unlink(device_name_.c_str()) == -1 && errno != ENOENT) {
-            BOOST_LOG_TRIVIAL(error) << "Failed to remove existing symlink: " << strerror(errno);
-            throw std::runtime_error("Failed to remove existing symlink");
-        }
-        BOOST_LOG_TRIVIAL(info) << "Existing symlink removed or not present";
+    // Open the slave pseudoterminal
+    int slave_fd = open(slave_name, O_RDWR);
+    if (slave_fd == -1) {
+        BOOST_LOG_TRIVIAL(error) << "Failed to create symlink for PTY slave: " << strerror(errno);
+        throw std::runtime_error("Failed to open the slave pseudoterminal");
+    }
 
-        BOOST_LOG_TRIVIAL(info) << "Slave PTY name: " << slave_name << std::endl;
 
-        // Open the slave pseudoterminal
-        int slave_fd;
-        slave_fd = open(slave_name, O_RDWR);
-        if (slave_fd == -1) {
-            std::cerr << "Error opening slave PTY: " << strerror(errno) << std::endl;
-            close();
-            return;
-        }
-        slave_fd_.assign(slave_fd);
 
-        if (symlink(slave_name, device_name_.c_str()) != 0) {
-            BOOST_LOG_TRIVIAL(error) << "Failed to create symlink for PTY slave: " << strerror(errno);
-            throw std::runtime_error("Failed to create symlink for PTY slave");
-        }
-        BOOST_LOG_TRIVIAL(info) << "Symlink for PTY slave created successfully";
+    chmod(device_name_.c_str(), 0660);
+    struct group* tty_grp = getgrnam("tty");
+    if (tty_grp && chown(device_name_.c_str(), -1, tty_grp->gr_gid) == -1) {
+        BOOST_LOG_TRIVIAL(error) << "Failed to change group of device: " << strerror(errno);
+        throw std::runtime_error("Failed to change group of device");
+    }
+    BOOST_LOG_TRIVIAL(info) << "Group changed successfully for the device";
 
-        chmod(device_name_.c_str(), 0660);
-        struct group* tty_grp = getgrnam("tty");
-        if (tty_grp && chown(device_name_.c_str(), -1, tty_grp->gr_gid) == -1) {
-            BOOST_LOG_TRIVIAL(error) << "Failed to change group of device: " << strerror(errno);
-            throw std::runtime_error("Failed to change group of device");
-        }
-        BOOST_LOG_TRIVIAL(info) << "Group changed successfully for the device";
-
-        master_fd_.assign(master_fd);
-        setup_pty(master_fd);
+    master_fd_.assign(master_fd);
+    slave_fd_.assign(slave_fd);
+    setup_pty(master_fd);
+    setup_pty(slave_fd);
 }
 
 void VirtualSerialPort::setup_pty(int fd) {
